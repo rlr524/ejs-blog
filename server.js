42098cdcdb69c555924e25db221759432aa08c2d
Rob Ranf <robranf@gmail.com>
2020-01-11 12:33:15 -0800

    adding base structure and css

--- server.js
+++ server.js
@@ -5,3 +5,9 @@ const app = express();
 
 app.set("view-engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.listen(process.env.PORT || 3000, () => {
+  let port = process.env.PORT || 3000;
+  console.log("Server is running on port " + port);
+});
